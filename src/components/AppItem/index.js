da2fa41d5acc4332e90579216c6a93b604ab3bca
laltha <156647324+laltha@users.noreply.github.com>
2024-04-18 23:00:20 +0530

    Update index.js

--- src/components/AppItem/index.js
+++ src/components/AppItem/index.js
@@ -1,13 +1,13 @@
 // Write your code here
-
 import './index.css'
 
 const AppItem = props => {
   const {appDetails} = props
   const {appName, imageUrl} = appDetails
+
   return (
     <li className="app-item">
-      <img src={imageUrl} alt={appName} className="appName" />
+      <img className="app-image" src={imageUrl} alt={appName} />
       <p className="app-name">{appName}</p>
     </li>
   )
